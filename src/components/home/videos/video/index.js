274c0d142e891471b18090805d4b6337ab899dcb
DESKTOP-NVB8L6C\SonNV <sonnguyen106mh@gmail.com>
2022-05-26 18:03:46 +0700

    adding youtube videos but error

--- src/components/home/videos/video/index.js
+++ src/components/home/videos/video/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { View, Text, ScrollView } from "react-native";
 import ContentShow from '../../../../utils/contentShow'
+import YouTube from "react-native-youtube";
 const VideoScreen = () => {
   return (
     <ScrollView>
-      
+      <YouTube apiKey="AIzaSyB4IZhZ2X3uUE3Rcqb7n58V-0w5Wc6BSUU"
+      videoId="x6ZeLDkvBmo" 
+      style={{alignSelf: 'stretch', height: 300}}/>
       <ContentShow/>
     </ScrollView>
   );
